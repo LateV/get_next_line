a39ade2e0c71136143b968038ef1670c36f79553
Vitalii <coulraven@ukr.net>
2017-12-04 00:42:56 +0200

    Add files via upload

---
+++ get_next_lineTest1.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrameau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/29 22:52:30 by jrameau           #+#    #+#             */
+/*   Updated: 2016/12/06 02:53:47 by jrameau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoinch(char const *s1, char c)
+{
+	char	*new_str;
+	size_t	i;
+	size_t	s1_len;
+
+	if (!s1 || !c)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	new_str = ft_strnew(s1_len + 1);
+	if (!new_str)
+		return (NULL);
+	i = -1;
+	while (++i < s1_len)
+		*(new_str + i) = *(s1 + i);
+	*(new_str + i) = c;
+	return (new_str);
+}
+
+
+int			ft_copyuntil(char **dst, char *src, char c)
+{
+	int		i;
+	int		count;
+	int		pos;
+
+	i = -1;
+	count = 0;
+	while (src[++i])
+		if (src[i] == c)
+			break ;
+	pos = i;
+	if (!(*dst = ft_strnew(i)))
+		return (0);
+	while (src[count] && count < i)
+	{
+		if (!(*dst = ft_strjoinch(*dst, src[count])))
+			return (0);
+		count++;
+	}
+	return (pos);
+}
+
+
+static t_list			*get_correct_file(t_list **file, int fd)
+{
+	t_list				*tmp;
+
+	tmp = *file;
+	while (tmp)
+	{
+		if ((int)tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew("", fd);
+	ft_lstadd(file, tmp);
+	tmp = *file;
+	return (tmp);
+}
+
+int						get_next_line(const int fd, char **line)
+{
+	char				buf[BUFF_SIZE + 1];
+	static t_list		*file;
+	int					i;
+	int					ret;
+	t_list				*curr;
+
+	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
+		return (-1);
+	curr = get_correct_file(&file, fd);
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[ret] = '\0';
+		(curr->content = ft_strjoin(curr->content, buf));
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	if (ret < BUFF_SIZE && !ft_strlen(curr->content))
+		return (0);
+	i = ft_copyuntil(line, curr->content, '\n');
+	printf("curr = %s\n", curr->content);
+	(i < (int)ft_strlen(curr->content))
+	 ? curr->content += (i + 1)
+		: ft_strclr(curr->content);
+	return (1);
+}
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		//ft_putendl(line);
+		free(line);
+	}
+	if (argc == 2)
+		close(fd);
+}
